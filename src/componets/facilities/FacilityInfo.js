50ccd7a439ede0e36f1c12350359643bb62fbb9b
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2021-03-27 20:43:29 +0100

    chore(master): facilities refactor

--- src/componets/facilities/FacilityInfo.js
+++ src/componets/facilities/FacilityInfo.js
@@ -1,8 +1,19 @@
 
 const FacilityInfo = ({ date, events }) => (
-  <div style={{}}>
+  <div style={{
+  }}>
     {date}
-    {events.map(({ event_name, duration }) => <div>{`${event_name} - ${duration}`}</div>)}
+    {events.map(({ event_name, duration, sessions }) =>
+      <div
+        style={{
+          marginTop: 16,
+          marginBotton: 16,
+        }}
+      >
+        <div>{`${event_name} - ${duration}`}</div>
+        <div>{sessions.map(session => <div>{`${session.from_hour} - ${session.to_hour}`}</div>)}</div>
+      </div>
+    )}
   </div>
 )
 export default FacilityInfo
