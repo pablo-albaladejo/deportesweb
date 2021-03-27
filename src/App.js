8c2e50eb0afb530b9a2bf916f7270bdd7017a430
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2021-03-27 17:59:26 +0100

    chore(master): apiFetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import { useEffect, useState } from 'react'
+import apiFetch, { FETCH_METHODS } from './utils/apiFetch';
+
+const API_URL = `https://deportesweb.madrid.es/CronosWebApi/api/v1`
 
 function App() {
   const [centers, setCenters] = useState()
 
   useEffect(() => {
-    setCenters([])
+    const fetchCenters = async () => {
+      const result = await apiFetch({
+        baseUrl: API_URL,
+        endPoint: `/events/enclosures/29/2021-03-29?session_token=`,
+        method: FETCH_METHODS.GET,
+        headers: {
+          'User-Agent': 'android/i2a',
+          'Host': 'deportesweb.madrid.es',
+          'Connection': 'Keep-Alive',
+          'Accept-Encoding': 'gzip',
+          'cache-control': 'no-cache',
+        }
+      })
+      setCenters(result)
+    }
+    fetchCenters()
   }, [])
 
   return (
---
+++ src/utils/apiFetch.js
@@ -0,0 +1,72 @@
+
+import { name } from '../../package.json';
+
+export const FETCH_METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+  PATCH: 'PATCH',
+};
+
+const apiFetch =
+  ({ baseUrl, endPoint, method, token = null, body = null, params = null, headers = null }) =>
+    new Promise((resolve, reject) => {
+      const isDev = process.env.NODE_ENV === 'development';
+
+      const defaultHeaders = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        //Authorization: token,
+      };
+
+      const opts = {
+        method,
+        headers: {
+          ...defaultHeaders,
+          ...headers,
+        },
+        body: body ? JSON.stringify(body) : null,
+      };
+
+      const urlParms = params ?
+        `?${Object.keys(params).map(key => `${key}=${params[key]}`).join('&')}`
+        :
+        '';
+
+      const uri = `${baseUrl}${endPoint || ''}${urlParms}`;
+
+      if (isDev) {
+        // eslint-disable-next-line no-console
+        console.log(`${name}::api::request`, {
+          opts,
+          uri,
+        });
+      }
+
+      fetch(uri, opts)
+        .then(response => {
+          if (response.ok) {
+            response.json().then(jsonResponse => {
+              // eslint-disable-next-line no-console
+              if (isDev) console.log(`${name}::api::jsonResponse`, jsonResponse);
+              resolve(jsonResponse);
+            }).catch(reject);
+          } else {
+            response.json()
+              .then(errJson => {
+                // eslint-disable-next-line no-console
+                if (isDev) console.log(`${name}::api::errJson`, errJson);
+                // eslint-disable-next-line prefer-promise-reject-errors
+                reject({ status: response.status, error: ((errJson.error) ? errJson.error : errJson) });
+              }).catch(reject);
+          }
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          if (isDev) console.log(`${name}::api::error`, error);
+          reject(error);
+        });
+    });
+
+export default apiFetch;
